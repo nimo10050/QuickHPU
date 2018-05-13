7d22ba1be8d2729b687193a5207a08a34039c2f5
nimo10050 <nimo10050@gmail.com>
2018-05-13 12:06:12 +0800

    创建班级回调接口

---
+++ app/src/main/java/com/iot/quickhpu/callback/NewClassCallback.java
@@ -0,0 +1,44 @@
+package com.iot.quickhpu.callback;
+
+import android.os.Handler;
+import android.os.Message;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+/**
+ * @Author m1563
+ * @Date 2018/3/1
+ * @Description
+ */
+
+public class NewClassCallback implements Callback {
+
+    private Handler mHandler;
+
+
+    public NewClassCallback(Handler handler) {
+        mHandler = handler;
+    }
+
+    @Override
+    public void onFailure(Call call, IOException e) {
+
+    }
+
+    @Override
+    public void onResponse(Call call, Response response) throws IOException {
+        if (response.code() == 200) {
+            String result = response.body().string();
+            if (result != null && !"".equals(result)) {
+                Message msg = new Message();
+                msg.what = 1;
+                msg.obj = result;
+                mHandler.sendMessage(msg);
+            }
+        }
+    }
+}
