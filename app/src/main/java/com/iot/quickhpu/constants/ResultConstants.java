fc2406e6c321253c473c3d1777a95b808b9b19d6
nimo10050 <nimo10050@gmail.com>
2018-05-16 19:54:24 +0800

    响应结果常量

---
+++ app/src/main/java/com/iot/quickhpu/constants/ResultConstants.java
@@ -0,0 +1,12 @@
+package com.iot.quickhpu.constants;
+
+/**
+ * @Author m1563
+ * @Date 2018/5/15
+ * @Description
+ */
+
+public class ResultConstants {
+
+    public static final int RESULT_OK = 1;
+}
