7ae53ef6620740373b34d989e6ae1ac5f717e98f
nimo10050 <nimo10050@gmail.com>
2018-05-16 22:35:13 +0800

    增加课程周次的切换

--- app/src/main/java/com/iot/quickhpu/fragment/ClassScheduleFragment.java
+++ app/src/main/java/com/iot/quickhpu/fragment/ClassScheduleFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.iot.quickhpu.R;
 import com.iot.quickhpu.pojo.MySubject;
@@ -20,16 +21,23 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * A simple {@link Fragment} subclass.
+ * @作者 LOZON
+ * @日期 2018/5/16
+ * @描述 课程表页面
  */
-public class ClassScheduleFragment extends Fragment {
+public class ClassScheduleFragment extends Fragment implements View.OnClickListener {
 
     private TimetableView mTimetableView;
+    private TextView tvPreWeek;
+    private TextView tvNextWeek;
+    private TextView tvCurrentWeek;
 
     private List<SubjectBean> subjectBeans;
 
+    private int currentWeek = 1;
+
     public ClassScheduleFragment() {
-        // Required empty public constructor
+
     }
 
 
@@ -37,20 +45,24 @@ public class ClassScheduleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View view=  inflater.inflate(R.layout.fragment_class_schedule, container, false);
+        View view = inflater.inflate(R.layout.fragment_class_schedule, container, false);
         // 课程表
-        mTimetableView=(TimetableView) view.findViewById(R.id.id_timetableView);
-        subjectBeans=transform(MySubjectModel.loadDefaultSubjects());
+        mTimetableView = (TimetableView) view.findViewById(R.id.id_timetableView);
+        tvPreWeek = view.findViewById(R.id.tv_pre_week);
+        tvNextWeek = view.findViewById(R.id.tv_next_week);
+        tvCurrentWeek = view.findViewById(R.id.tv_current_week);
+
+        tvCurrentWeek.setText("第" + currentWeek + " 周");
+
+        tvPreWeek.setOnClickListener(this);
+        tvNextWeek.setOnClickListener(this);
+
+        subjectBeans = transform(MySubjectModel.loadDefaultSubjects());
         mTimetableView.setDataSource(subjectBeans)
-                .setCurTerm("大三上学期")
-                .setCurWeek(1)
+                //.setCurTerm("大三上学期")
+                .setCurWeek(currentWeek)
                 //.setOnSubjectItemClickListener(getActivity())
                 .showTimetableView();
-
-        //调用过showSubjectView后需要调用changWeek()
-        //第二个参数为true时在改变课表布局的同时也会将第一个参数设置为当前周
-        //第二个参数为false时只改变课表布局
-        //mTimetableView.changeWeek(curWeek, true);
 
         return view;
     }
@@ -76,10 +88,36 @@ public class ClassScheduleFragment extends Fragment {
                 colorCount++;
             }
             //转换
-            subjectBeans.add(new SubjectBean(mySubject.getName(), mySubject.getRoom(),mySubject.getTeacher(), mySubject.getWeekList(),
-                    mySubject.getStart(), mySubject.getStep(), mySubject.getDay(), color,mySubject.getTime()));
+            subjectBeans.add(new SubjectBean(mySubject.getName(), mySubject.getRoom(), mySubject.getTeacher(), mySubject.getWeekList(),
+                    mySubject.getStart(), mySubject.getStep(), mySubject.getDay(), color, mySubject.getTime()));
         }
         return subjectBeans;
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.tv_pre_week:
+                currentWeek = (--currentWeek) < 1 ? 1 : currentWeek;
+                tvCurrentWeek.setText("第" + currentWeek + " 周");
+                changeWeek(currentWeek);
+                break;
+            case R.id.tv_next_week:
+                currentWeek = (++currentWeek) > 22 ? 22 : currentWeek;
+                tvCurrentWeek.setText("第" + currentWeek + " 周");
+                changeWeek(currentWeek);
+                break;
+        }
+    }
+
+    private void changeWeek(int week) {
+        subjectBeans = transform(MySubjectModel.loadDefaultSubjects());
+        mTimetableView.setDataSource(subjectBeans)
+                //.setCurTerm("大三上学期")
+                .setCurWeek(week)
+                //.setOnSubjectItemClickListener(getActivity())
+                .showTimetableView();
     }
 
 }
