a064ce5964bde4a7a5dcf2233e0cd7a3b79114d7
nimo10050 <nimo10050@gmail.com>
2018-05-17 22:42:28 +0800

    update

--- app/src/main/java/com/iot/quickhpu/utils/CommonUtils.java
+++ app/src/main/java/com/iot/quickhpu/utils/CommonUtils.java
@@ -1,5 +1,13 @@
 package com.iot.quickhpu.utils;
 
+import android.app.Activity;
+import android.content.Context;
+import android.os.IBinder;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
@@ -36,5 +44,44 @@ public class CommonUtils {
         String str = millis + String.format("%02d", end2);
         long id = new Long(str);
         return id;
+    }
+
+    /**
+     * 点击屏幕空白处隐藏键盘
+     * @param event
+     * @param view
+     * @param activity
+     */
+
+    public static Boolean hideInputMethod(Context context, View v) {
+        InputMethodManager imm = (InputMethodManager) context
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm != null) {
+            return imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+        }
+        return false;
+    }
+
+    /**
+     * 判断当前点击屏幕的地方是否是软键盘
+     * @param v
+     * @param event
+     * @return
+     */
+    public static boolean isShouldHideInput(View v, MotionEvent event) {
+        if (v != null && (v instanceof EditText)) {
+            int[] leftTop = { 0, 0 };
+            v.getLocationInWindow(leftTop);
+            int left = leftTop[0], top = leftTop[1], bottom = top + v.getHeight(), right = left
+                    + v.getWidth();
+            if (event.getX() > left && event.getX() < right
+                    && event.getY() > top && event.getY() < bottom) {
+                // 保留点击EditText的事件
+                return false;
+            } else {
+                return true;
+            }
+        }
+        return false;
     }
 }
