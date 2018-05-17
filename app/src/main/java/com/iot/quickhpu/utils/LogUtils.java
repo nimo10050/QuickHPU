547e1b55220bf82d3dc03f6fc7b7e890ec202c06
nimo10050 <nimo10050@gmail.com>
2018-05-17 22:43:32 +0800

    update

--- app/src/main/java/com/iot/quickhpu/utils/LogUtils.java
+++ app/src/main/java/com/iot/quickhpu/utils/LogUtils.java
@@ -12,7 +12,7 @@ public class LogUtils
     }  
   
     private static boolean isDebug = true;// 是否需要打印bug，可以在application的onCreate函数里面初始化
-    private static final String TAG = "[HPU日志报告]  :  ";
+    private static final String TAG = "[ HPU日志报告 ]  :  ";
   
     // 下面四个是默认tag的函数  
     public static void i(String msg)  
